cd6733654f9c7e4fd530d0bb5fc0f006ba856ea3
routhy46 <37312600+chum46@users.noreply.github.com>
2018-10-31 02:51:17 -0500

    liri is responding to all 4 commands successfully

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var request = require('request');
+var fs = require('file-system');
 var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -12,13 +14,20 @@ var getTweets = function () {
             // console.log(tweets);
             // Attributes we want: 'text' associated with it and the 'created_at'
             // Loop through all of the tweets and then console log the attributes
+            console.log('----------------------');
             for (var i = 0; i < tweets.length; i++) {
+                
                 console.log(tweets[i].created_at);
                 console.log(' ');
                 console.log(tweets[i].text);
+                console.log('----------------------');
             }
         }
     });
+}
+
+var getArtists = function (artist) {
+    return artist.name;
 }
 
 var getSong = function (songName) {
@@ -27,7 +36,50 @@ var getSong = function (songName) {
             return console.log('Error occurred: ' + err);
         }
         // Look at the first object
-        console.log(data.tracks.items[0]);
+        // console.log(data.tracks.items[0]);
+
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+            console.log(i);
+            console.log('artist(s): ' + songs[i].artists.map(
+                getArtists));
+            console.log('song name: ' + songs[i].name);
+            console.log('preview song: ' + songs[i].preview_url);
+            console.log('album: ' + songs[i].album.name);
+            console.log('----------------------------------');
+        }
+    });
+}
+
+var getMovie = function (movieName) {
+    request('http://www.omdbapi.com/?t=' + movieName + '&apikey=trilogy', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var jsonData = JSON.parse(body);
+            console.log('Title: ' + jsonData.Title);
+            console.log('Year: ' + jsonData.Year);
+            console.log('IMDB Rating: ' + jsonData.Ratings[0].Value);
+            console.log('Rotten tomato rating: ' + jsonData.Ratings[1].Value);
+            console.log('Country: ' + jsonData.Country);
+            console.log('Language: ' + jsonData.Language);
+            console.log('Plot: ' + jsonData.Plot);
+            console.log('Actors: ' + jsonData.Actors);
+        }
+    });
+}
+
+var doWhatItSays = function () {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+
+        // Create an array of 2 indices from file split with a comma
+
+        var dataArray = data.split(',');
+        
+        if (dataArray.length == 2) {
+            userQuery(dataArray[0], dataArray[1]);
+        } else if (dataArray.length == 1) {
+            userQuery(dataArray[0]);
+        }
     });
 }
 
@@ -37,8 +89,14 @@ var userQuery = function (caseData, functionData) {
         case 'my-tweets':
             getTweets();
             break;
-        case "spotify-this-song":
+        case 'spotify-this-song':
             getSong(functionData);
+            break;
+        case 'movie-this':
+            getMovie(functionData);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
             break;
         default:
             console.log('LIRI has no data to return')
